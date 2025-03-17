b0872a56a916b3310518ebc4fe27249891e31e64
Shuriky <hdoo0027@student.monash.edu>
2025-03-17 21:06:29 +1100

    08 and 10

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let result = 0;
+  for (let i = 0; i < array.length; i++) {
+    result += array[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  for (let i = 0; i < array.length; i++) {
+    result *= array[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) {
+    return 1;
+  }
+	return a * factorial(a - 1);
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (x < 0) return "OOPS";
+    sqrt_5 = Math.sqrt(5);
+    return Math.round((Math.pow((1 + sqrt_5) / 2, x) - Math.pow((1 - sqrt_5) / 2, x)) / sqrt_5);
 };
 
 // Do not edit below this line
