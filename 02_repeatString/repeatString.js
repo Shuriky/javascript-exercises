ad08d66af1ea64d1fb6c62d8de108a0079c5aaa7
Shuriky <hdoo0027@student.monash.edu>
2025-03-17 20:25:14 +1100

    01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(myString, times) {
+    if (times < 0) {
+        return 'ERROR';
+    }
+    result = '';
+    for (let i = 0; i < times; i++) {
+        result += myString;
+    }
+    return result;
 };
 
 // Do not edit below this line
